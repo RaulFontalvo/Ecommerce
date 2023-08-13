723a6844f1d1372c94ae5afad1a9fc98fbeccc47
Raul Fontalvo <rfontalvo77@yahoo.com>
2023-08-13 00:32:58 -0500

    Función para cambiar el color del encabezado

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function changeBackgroundColor() {
     const currentColor = window.getComputedStyle(body).backgroundColor;
     body.style.backgroundColor = currentColor === 'rgb(255, 255, 255)' ? '#f2f2f2' : '#fff';
 }
+
+// Función para cambiar el color del encabezado
+function changeHeadingColor() {
+    const mainHeading = document.getElementById('main-heading');
+    const currentColor = window.getComputedStyle(mainHeading).color;
+    mainHeading.style.color = currentColor === 'rgb(255, 255, 255)' ? '#333' : '#fff';
+}
